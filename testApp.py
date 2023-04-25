2f3f4b3bbe4265960046735973b69798a613216a
Wipfla <marvin.wipfler@web.de>
2023-04-25 15:59:55 +0200

    test

--- testApp.py
+++ testApp.py
@@ -3,4 +3,11 @@ import pandas as pd
 import numpy as np
 
 st.title('Test App')
-print(1337)
+
+# Read the JSON file into a Pandas dataframe
+df = pd.read_json('data/data2.json')
+
+# Print the dataframe
+print(df)
+
+
