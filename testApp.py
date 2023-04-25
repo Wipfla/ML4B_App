358ac75a18289b865a5493cecbd2f520bfd520b5
Wipfla <117998474+Wipfla@users.noreply.github.com>
2023-04-25 16:09:42 +0200

    Update testApp.py

--- testApp.py
+++ testApp.py
@@ -5,7 +5,7 @@ import numpy as np
 st.title('Test App')
 
 # Read the JSON file into a Pandas dataframe
-df = pd.read_json('data/data2.json')
+# df = pd.read_json('data/data2.json')
 
 # Print the dataframe
 # print(df)
