08e705d1cdc6ef19fb828fdd963b60a498f7a340
Wipfla <117998474+Wipfla@users.noreply.github.com>
2023-04-25 16:22:44 +0200

    Update testApp.py

--- testApp.py
+++ testApp.py
@@ -2,13 +2,21 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+import json
+
 st.title('Test App')
 
-
+#mit Pandas, geht noch nicht
 # Read the JSON file into a Pandas dataframe
-df = pd.read_json('data/data2.json')
+#df = pd.read_json('data/data2.json')
 
 # Print the dataframe
 # print(df)
 
+#### normaler weg
+# Open the JSON file and load its contents into a variable
+with open('data/data2.json', 'r') as d2:
+    data2 = json.load(d2)
 
+# Print the contents of the JSON file
+print(data2)
