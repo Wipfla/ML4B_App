e4bd50d1d1cfa31f5eb9b10baf599c352ae9cee9
Wipfla <marvin.wipfler@web.de>
2023-04-28 13:04:53 +0200

    yee

--- testApp.py
+++ testApp.py
@@ -16,20 +16,29 @@ st.write('Es funktioniert noch nicht wirklich, Irgendwie bricht er immer bei den
 # print(df)
 
 
-def load_data(nrows):
-    with open('data/data2.json', nrows=nrows) as d2:
-        data2 = json.load(d2)
-    return data2
+# Laden Sie Ihren DataFrame
+df2 = pd.read_json('data/data2.json')
+
+# Zeigen Sie den DataFrame in einer Tabelle an
+st.table(df2)
+
+# Zeigen Sie den DataFrame im DataFrame-Viewer an
+st.dataframe(df2)
+
+# # def load_data(nrows):
+# #     with open('data/data2.json', nrows=nrows) as d2:
+# #         data2 = json.load(d2)
+# #     return data2
 
 
-data = load_data()
-st.write(data)
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data(100)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text('Loading data...done!')
+# #data = load_data()
+# #st.write(data)
+# # Create a text element and let the reader know the data is loading.
+# data_load_state = st.text('Loading data...')
+# # Load 10,000 rows of data into the dataframe.
+# data = load_data(100)
+# # Notify the reader that the data was successfully loaded.
+# data_load_state.text('Loading data...done!')
 
-st.subheader('Raw data')
-st.write(data)
+# st.subheader('Raw data')
+# st.write(data)
