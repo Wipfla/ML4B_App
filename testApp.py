ba9fa7c05bb8a1486c41394c36d0675eda75ba5d
Wipfla <marvin.wipfler@web.de>
2023-04-30 20:30:05 +0200

    fgh

--- testApp.py
+++ testApp.py
@@ -8,7 +8,8 @@ st.title('Test App')
 st.write('Es funktioniert noch nicht wirklich, Irgendwie bricht er immer bei den Pandas Befehlen ab, kp warum.')
 
 # Lade DataFrame
-df2 = pd.read_json('ML4B_App/data/data2/data2.json')
+#df2 = pd.read_json('ML4B_App/data/data2/data2.json')
+df2 = pd.read_csv('data\data2\Data2_Accelerometer_data.csv')
 ## Pierre: vielleicht ist der Path nicht richtig zur data2 und kann das deswegen nicht lesen/nicht im gleichen Directory wie der Code -> Error
 
 # Zeig DataFrame in einer Tabelle an
