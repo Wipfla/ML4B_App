704efb6bb7b7f5d4ba8b9160ae380b09b74031fc
Wipfla <marvin.wipfler@web.de>
2023-04-30 20:35:21 +0200

    it works!

--- testApp.py
+++ testApp.py
@@ -13,25 +13,7 @@ df2 = pd.read_csv('data/data2/Data2_Accelerometer_data.csv')
 ## Pierre: vielleicht ist der Path nicht richtig zur data2 und kann das deswegen nicht lesen/nicht im gleichen Directory wie der Code -> Error
 
 # Zeig DataFrame in einer Tabelle an
-st.table(df2)
+#st.table(df2)
 
 # Zeig DataFrame im DataFrame-Viewer an
 st.dataframe(df2)
-
-# # def load_data(nrows):
-# #     with open('data/data2.json', nrows=nrows) as d2:
-# #         data2 = json.load(d2)
-# #     return data2
-
-
-# #data = load_data()
-# #st.write(data)
-# # Create a text element and let the reader know the data is loading.
-# data_load_state = st.text('Loading data...')
-# # Load 10,000 rows of data into the dataframe.
-# data = load_data(100)
-# # Notify the reader that the data was successfully loaded.
-# data_load_state.text('Loading data...done!')
-
-# st.subheader('Raw data')
-# st.write(data)
