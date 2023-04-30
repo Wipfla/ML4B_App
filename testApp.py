b078a6d2b1ebfc1ae346ed1f93da7c03c8edf97c
Wipfla <marvin.wipfler@web.de>
2023-04-30 20:18:46 +0200

    rft

--- testApp.py
+++ testApp.py
@@ -8,7 +8,7 @@ st.title('Test App')
 st.write('Es funktioniert noch nicht wirklich, Irgendwie bricht er immer bei den Pandas Befehlen ab, kp warum.')
 
 # Lade DataFrame
-df2 = pd.read_json('data/data2/data2.json')
+df2 = pd.read_json('ML4B_App/data/data2/data2.json')
 ## Pierre: vielleicht ist der Path nicht richtig zur data2 und kann das deswegen nicht lesen/nicht im gleichen Directory wie der Code -> Error
 
 # Zeig DataFrame in einer Tabelle an
