fb19d70c7896b60b75259e5ff89547f0ab5d013f
Wipfla <marvin.wipfler@web.de>
2023-04-30 21:24:09 +0200

    BarChart

--- testApp.py
+++ testApp.py
@@ -9,14 +9,13 @@ st.write('It works! :) Hallelujah!')
 
 # Lade DataFrame
 #df2 = pd.read_json('ML4B_App/data/data2/data2.json')
-df2 = pd.read_csv('data/data2/Data2_Accelerometer_data.csv')
-## Pierre: vielleicht ist der Path nicht richtig zur data2 und kann das deswegen nicht lesen/nicht im gleichen Directory wie der Code -> Error
+df_walk_Acc = pd.read_csv('data/NormalWalk/Accelerometer.csv')
 
 # Zeig DataFrame in einer Tabelle an
-#st.table(df2)
+#st.table(df_walk_Acc)
 
 # Zeig DataFrame im DataFrame-Viewer an
-st.dataframe(df2)
+st.dataframe(df_walk_Acc)
 
 # Zeig DataFrame als Bar Chart an
-st.bar_chart(data =df2, width=20, height=10, use_container_width=True)
+st.bar_chart(data =df_walk_Acc, width=20, height=10, use_container_width=True)
