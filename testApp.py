1bb0d0251d0098e45d874bc79c51653452fda3ba
Wipfla <marvin.wipfler@web.de>
2023-05-02 17:33:29 +0200

    Plot Data Gyro

--- testApp.py
+++ testApp.py
@@ -22,7 +22,8 @@ st.dataframe(df_JJ_Gyr)
 st.button('Click me!',on_click=st.balloons)
 
 # Zeig DataFrame als Line Chart an
-st.line_chart(data =df_JJ_Gyr, x='time', y=['x','y','z'], title='Gyroscope Data')
+st.caption('Gyroscope Data')
+st.line_chart(data =df_JJ_Gyr, x='time', y=['x','y','z'])
 
 #Frage
 activity = st.radio(
