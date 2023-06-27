f1c99e3511c13eab0e09b1f5fe47537f6c401756
pierrengel <pierre.engel@fau.de>
2023-06-27 20:25:53 +0200

    Fine tuning

--- testApp.py
+++ testApp.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle as pkl
-from my_functions import getSensorData, getMetricsAcc, getMetricsGyr, getMetricsOri, getMetrics, generate_playlist, generate_video
+from my_functions import getSensorData, getMetricsAcc, getMetricsGyr, getMetricsOri, getMetrics, generate_playlists
 
 import streamlit as st
 
