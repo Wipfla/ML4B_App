6bad785b3e78f61453d75f3fd452182ff94557a3
Wipfla <marvin.wipfler@web.de>
2023-06-28 16:30:03 +0200

    video seite

--- testApp.py
+++ testApp.py
@@ -67,7 +67,7 @@ def page2():
         st.write(f'Basierend auf deinen Bewegungsdaten hast du **:red[{prediction}]** gemacht!')
 
                 
-    
+        #Korrekt? Ja/Nein
         st.subheader("War dies Korrekt?")    
         container_yes, container_no = st.columns(2)
         
@@ -133,11 +133,51 @@ def page3():
         prediction = predcit(UserFile_df)
         st.write(f'Basierend auf deinen Bewegungsdaten hast du **:red[{prediction}]** gemacht!')
 
+        #Korrekt? Ja/Nein
+        st.subheader("War dies Korrekt?")    
+        container_yes, container_no = st.columns(2)
         
+        with container_yes:
+            yesButton = st.button(label = 'Ja', use_container_width = 1)
+            
+        with container_no:
+            noButton = st.button(label = 'Nein', use_container_width = 1)
+                    
+        if yesButton:
+            st.session_state['inCorrect'] = False
+            st.success("Super dann starte deine Playlist!", icon="💪")
+            selected_link = generate_video(prediction)
+            st.write(f"Hier ist deine persönlich ausgesuchte Playlist:")
+            st.markdown(f"[Playlist Link]({selected_link})")
+
+        if 'inCorrect' not in st.session_state:
+            st.session_state['inCorrect'] = False
+            
+        if noButton or st.session_state['inCorrect']:
+            st.session_state['inCorrect'] = True  
+            st.subheader("Oh, kannst du uns verraten was die richtige Antwort war?")
+            input1, input2, input3 = st.columns(3)
+            
+            with input1:
+                jjButton = st.button(label = 'Jumping Jacks', use_container_width = 1)
+                if jjButton:
+                    st.write(f"Hier ist deine angepasste persönlich ausgesuchte Playlist:")
+                    link = generate_video('jumpingjacks')
+                    st.markdown(f"[Playlist Link]({link})")
+            with input2:
+                pushupButton = st.button(label = 'PushUps', use_container_width = 1)
+                if pushupButton:
+                    st.write(f"Hier ist deine angepasste persönlich ausgesuchte Playlist:")
+                    link = generate_video('PushUps')
+                    st.markdown(f"[Playlist Link]({link})")
+            with input3:
+                walkingButton = st.button(label = 'Walking', use_container_width = 1)
+                if walkingButton:
+                    st.write(f"Hier ist deine angepasste persönlich ausgesuchte Playlist:")
+                    link = generate_video('walking')
+                    st.markdown(f"[Playlist Link]({link})")
         
-        videoURL = generate_video(UserFile_df)
-        
-        st.video(videoURL)
+        st.video(link)
 
 
 
