afcca7fe99168656aec88cf8091848a1c756b2b6
pierrengel <pierre.engel@fau.de>
2023-06-28 09:11:25 +0200

    Button update

--- testApp.py
+++ testApp.py
@@ -85,9 +85,9 @@ def page2():
                 st.success("Playlist gefunden!")
                 #st.write(f"Hier ist deine persönlich ausgesuchte Playlist: {selected_link}")
 
-            if st.button("Hör direkt rein!"): #button funktioniert noch nicht, leitet nicht weiter
+                if st.button("Hör direkt rein!"): #button funktioniert noch nicht, leitet nicht weiter
                 #st.write(f"Du wirst weitergeleitet zu: {selected_link}")
-                webbrowser.open_new_tab(selected_link)
+                    webbrowser.open_new_tab(selected_link)
             else:
                 st.warning("No playlist available for the selected category.")
 
