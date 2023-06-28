828bae7678713fdeee5b7f9c2954f90ad668c49d
pierrengel <pierre.engel@fau.de>
2023-06-28 13:55:59 +0200

    Stattstik

--- testApp.py
+++ testApp.py
@@ -180,6 +180,28 @@ def page4():
             st.write('Beschleunigung im Mittelwert: ', acc_mean)
         with st.container():
             st.write('Beschleunigung im Median: ', acc_med)
+
+    options = ["Maximale Höhe", "Minimale Höhe", "Höhe im Mittelwert", "Höhe im Median", "Maximale Beschleunigung", "Minimale Beschleunigung", "Beschleunigung im Mittelwert", "Beschleunigung im Median"]
+    selected_option = st.selectbox('Was willst du herausfinden?', options)
+
+    if selected_option == "Maximale Höhe":
+        st.write('Maximale Höhe: ', gyro_max)
+    elif selected_option == "Minimale Höhe":
+        st.write('Minimale Höhe: ', gyro_min)
+    elif selected_option == "Höhe im Mittelwert":
+        st.write('Höhe im Mittelwert: ', gyro_mean)
+    elif selected_option == "Höhe im Media":
+        st.write('Höhe im Median: ', gyro_med)
+    elif selected_option == "Maximale Beschleunigung":
+        st.write('Maximale Beschleunigung: ', acc_max)
+    elif selected_option == "Minimale Beschleunigung":
+        st.write('Minimale Beschleunigung: ', acc_min)
+    elif selected_option == "Beschleunigung im Mittelwert":
+        st.write('Beschleunigung im Mittelwert: ', acc_mean)
+    elif selected_option == "Beschleunigung im Media":
+        st.write('Beschleunigung im Median: ', acc_med)
+    else:
+        st.write("Please select an option.")
         
         
         
