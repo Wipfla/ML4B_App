d623222c2a6fc9125dbf5bca2f1c2f8b4336e755
pierrengel <pierre.engel@fau.de>
2023-06-29 11:22:14 +0200

    stat farbe

--- my_functions.py
+++ my_functions.py
@@ -230,7 +230,7 @@ def create_combined_scatter_plot(data_list):
     scatter_plot = alt.Chart(df_long).mark_circle(size=60).encode(
         x='Variable',
         y='Values',
-        color=alt.Color('Variable:N', scale=None, legend=None, scale=alt.Scale(domain=list(color_map.keys()), range=list(color_map.values())))
+        color=alt.Color('Variable:N', legend=None, scale=alt.Scale(domain=list(color_map.keys()), range=list(color_map.values())))
         tooltip=['Variable', 'Values']
     ).properties(
         width=600,
