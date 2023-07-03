711c84bb6860fddae8e977917227b74ad13f75a0
pierrengel <pierre.engel@fau.de>
2023-07-03 20:20:51 +0200

    statttt

--- my_functions.py
+++ my_functions.py
@@ -220,17 +220,21 @@ def create_combined_scatter_plot(data_list):
     summary_stats = summary_stats.rename(columns={'index': 'Variable', 'mean': 'Mean', 'median': 'Median'})
 
     # Create the combined scatter plot
-    scatter_circles = alt.Chart(df_long).mark_point(size=60, filled=True).encode(
+    scatter_circles = alt.Chart(df_long).transform_calculate(
+        Variable_Label="datum.Variable == 'x' ? '0' : datum.Variable == 'y' ? '1' : '2'"
+    ).mark_point(size=60, filled=True).encode(
         x=alt.X('Variable:N', axis=alt.Axis(values=['x', 'y', 'z'])),
         y='Values',
-        fill=alt.Color('Variable:N'),
+        fill=alt.Color('Variable_Label:N'),
         tooltip=[alt.Tooltip('Variable', title='Variable'), alt.Tooltip('Values', title='Value')]
     )
 
-    scatter_points = alt.Chart(df_long).mark_point(size=100, filled=True).encode(
+    scatter_points = alt.Chart(df_long).transform_calculate(
+        Variable_Label="datum.Variable == 'x' ? '0' : datum.Variable == 'y' ? '1' : '2'"
+    ).mark_point(size=100, filled=True).encode(
         x=alt.X('Variable:N', axis=alt.Axis(values=['x', 'y', 'z'])),
         y='Values',
-        fill=alt.Color('Variable:N'),
+        fill=alt.Color('Variable_Label:N'),
         tooltip=[alt.Tooltip('Variable', title='Variable'), alt.Tooltip('Values', title='Value')]
     )
 
