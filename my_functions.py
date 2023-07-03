65e12cbebe55fed16e185d4be57defe8cda08732
pierrengel <pierre.engel@fau.de>
2023-07-03 20:01:23 +0200

    statttt

--- my_functions.py
+++ my_functions.py
@@ -223,7 +223,6 @@ def create_combined_scatter_plot(data_list):
     scatter_circles = alt.Chart(df_long).mark_point(size=60).encode(
         x=alt.X('Variable:N', axis=alt.Axis(values=['x', 'y', 'z'])),
         y='Values',
-        color=alt.Color('Variable:N'),
         tooltip=[alt.Tooltip('Variable', title='Variable'), alt.Tooltip('Values', title='Value')]
     )
 
